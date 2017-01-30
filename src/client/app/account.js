f21e15c37963bc86a67f027527448b3107bc60f1
kalvyn-lu <kalvynlu@gmail.com>
2017-01-29 21:44:51 -0800

    started on Account class

---
+++ src/client/app/account.js
@@ -0,0 +1,13 @@
+class Account {
+	constructor() {
+		this.data = {
+			hashId = "",
+			liked = []
+		}
+	}
+
+	//Returns true if contained, returns false if not
+	function login (username, password) {
+		
+	}
+}
