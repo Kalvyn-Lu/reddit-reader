fbeb07da67101737fcc5130b8505ff903ed55722
Kalvyn Lu <kalvynlu@gmail.com>
2017-02-01 05:56:26 +0000

    cross origin

--- src/server.js
+++ src/server.js
@@ -31,6 +31,13 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/account', (req, res)=> {
   var hashed = tempHash(req.body.username, req.body.password);
   if(tempAccountMap[hashed]) {
